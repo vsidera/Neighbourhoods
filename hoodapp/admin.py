3facd40959c28346a959f19fede160e09e5d635d
vsidera <siderravictor@gmail.com>
2020-10-31 08:12:54 +0300

    add models to admin.py

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Neighbourhood, Profile, Business, Post
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Neighbourhood)
